8cd068db72b08f70e3d9a45c2861226434af78a7
timbu <tim.buckland@student.com>
2018-09-23 13:57:03 +0100

    do source-tracking first

--- salesforce/service_versions.py
+++ salesforce/service_versions.py
@@ -451,8 +451,8 @@ class SalesforceService:
 
 
 # BASIC UP AND DOWN SYNC
-# SKIP DUPLICATES
 # SOURCE TRACKING
+# SKIP DUPLICATES
 
 
 from nameko.rpc import RpcProxy
@@ -508,8 +508,9 @@ class SalesforceService:
 
 
 
+
 # BASIC UP AND DOWN SYNC
-# SKIP DUPLICATES
+# SOURCE TRACKING
 
 
 from nameko.rpc import RpcProxy
@@ -518,11 +519,7 @@ from nameko.events import event_handler
 from nameko_salesforce.streaming import handle_sobject_notification
 from nameko_salesforce.api import SalesforceAPI
 
-from platform_lock.dependencies.lock import DistributedLock
-
-
-def skip_duplicate_key(sobject_type, record_type, notification):
-    return 'salesforce:skip_duplicate({})'.format(notification['event']['replayId'])
+from source_tracker import SourceTracker
 
 
 class SalesforceService:
@@ -533,10 +530,15 @@ class SalesforceService:
 
     salesforce = SalesforceAPI()
 
-    lock = DistributedLock()
+    source_tracker = SourceTracker()
 
     @event_handler('contacts', 'contact_created')
     def handle_platform_contact_created(self, payload):
+
+        if self.source_tracker.is_sourced_from_salesforce():
+            print("Ignoring event that was sourced from salesforce")
+            return
+
         result = self.salesforce.Contact.create(
             {'LastName': payload['contact']['name']}
         )
@@ -546,13 +548,12 @@ class SalesforceService:
         'Contact', exclude_current_user=True,
         notify_for_operation_update=False
     )
-    @lock.skip_duplicates(key=skip_duplicate_key)
     def handle_sf_contact_created(self, sobject_type, record_type, notification):
-        contact = self.contacts_rpc.create_contact(
-            {'name': notification['sobject']['Name']}
-        )
+        with self.source_tracker.sourced_from_salesforce():
+            contact = self.contacts_rpc.create_contact(
+                {'name': notification['sobject']['Name']}
+            )
         print('Created {} on platform'.format(contact))
-
 
 
 
